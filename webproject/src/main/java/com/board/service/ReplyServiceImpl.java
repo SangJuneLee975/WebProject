cc6047567bb4ed01c060ea18338873751b4cd348
pkok9 <pkok9@DESKTOP-NPPRJ49>
2020-11-16 14:29:56 +0900

    commit

---
+++ webproject/src/main/java/com/board/service/ReplyServiceImpl.java
@@ -0,0 +1,49 @@
+package com.board.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.board.domain.ReplyVO;
+import com.board.persistence.ReplyDAO;
+
+@Service
+public class ReplyServiceImpl implements ReplyService {
+
+	@Inject
+	private ReplyDAO dao;
+
+	// ´ñ±Û Á¶È¸
+	@Override
+	public List<ReplyVO> readReply(int bno) throws Exception {
+		return dao.readReply(bno);
+	}
+
+	// ´ñ±Û ÀÛ¼º
+	@Override
+	public void writeReply(ReplyVO vo) throws Exception {
+		dao.writeReply(vo);		
+	} 
+
+	// Æ¯Á¤ ´ñ±Û Á¶È¸
+	@Override
+	public ReplyVO readReplySelect(int rno) throws Exception {
+		return dao.readReplySelect(rno);
+	}
+	
+	// ´ñ±Û ¼öÁ¤
+	@Override
+	public void replyUpdate(ReplyVO vo) throws Exception {
+		dao.replyUpdate(vo);
+	}
+
+	// ´ñ±Û »èÁ¦
+	@Override
+	public void replyDelete(ReplyVO vo) throws Exception {
+		dao.replyDelete(vo);
+	}
+
+ 
+} 
